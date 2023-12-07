ee3e9fb19bc1bfc02be99dfa322d56fc994a2e0f
Andy Pfister <andy.pfister@simplificator.com>
2023-12-07 11:24:52 +0100

    Add basic deployment action

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -33,12 +33,18 @@ describe('action', () => {
     setOutputMock = jest.spyOn(core, 'setOutput').mockImplementation()
   })
 
-  it('sets the time output', async () => {
+  it('deploys Hashicorp', async () => {
     // Set the action's inputs as return values from core.getInput()
     getInputMock.mockImplementation((name: string): string => {
       switch (name) {
-        case 'milliseconds':
-          return '500'
+        case 'compose-file':
+          return 'docker-compose.test.yml'
+        case 'stack-name':
+          return 'david'
+        case 'ssh-user-at-host':
+          return 'david@127.0.0.1'
+        case 'ssh-port':
+          return '2222'
         default:
           return ''
       }
@@ -46,44 +52,5 @@ describe('action', () => {
 
     await main.run()
     expect(runMock).toHaveReturned()
-
-    // Verify that all of the core library functions were called correctly
-    expect(debugMock).toHaveBeenNthCalledWith(1, 'Waiting 500 milliseconds ...')
-    expect(debugMock).toHaveBeenNthCalledWith(
-      2,
-      expect.stringMatching(timeRegex)
-    )
-    expect(debugMock).toHaveBeenNthCalledWith(
-      3,
-      expect.stringMatching(timeRegex)
-    )
-    expect(setOutputMock).toHaveBeenNthCalledWith(
-      1,
-      'time',
-      expect.stringMatching(timeRegex)
-    )
-    expect(errorMock).not.toHaveBeenCalled()
-  })
-
-  it('sets a failed status', async () => {
-    // Set the action's inputs as return values from core.getInput()
-    getInputMock.mockImplementation((name: string): string => {
-      switch (name) {
-        case 'milliseconds':
-          return 'this is not a number'
-        default:
-          return ''
-      }
-    })
-
-    await main.run()
-    expect(runMock).toHaveReturned()
-
-    // Verify that all of the core library functions were called correctly
-    expect(setFailedMock).toHaveBeenNthCalledWith(
-      1,
-      'milliseconds not a number'
-    )
-    expect(errorMock).not.toHaveBeenCalled()
   })
 })
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import * as core from '@actions/core'
-import { wait } from './wait'
+import { execSync } from 'child_process'
 
 /**
  * The main function for the action.
@@ -7,18 +7,26 @@ import { wait } from './wait'
  */
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
+    const composeFile = core.getInput('compose-file')
+    const stackName = core.getInput('stack-name')
+    const sshUserAtHost = core.getInput('ssh-user-at-host')
+    const sshPort = core.getInput('ssh-port')
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    core.info("Creating docker context ...")
+    execSync(`docker context create target --docker "host=ssh://${sshUserAtHost}:${sshPort}"`, { stdio: [] })
+    execSync(`docker context use target`)
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    core.info("Initialising Swarm if required ...")
+    execSync("docker node ls || docker swarm init", { stdio: [] })
 
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    core.info("Deploying stack ...")
+    execSync(`docker stack deploy --compose-file ${composeFile} --prune --with-registry-auth ${stackName}`, { stdio: [] })
+
+    core.info("Waiting for deployment to complete ...")
+    execSync(`docker run --rm -i -v $(pwd)/${composeFile}:/docker-compose.yml -v /var/run/docker.sock:/var/run/docker.sock sudobmitch/docker-stack-wait:v0.2.5 -l "--since 2m" -t 120 ${stackName}`, { stdio: [] })
+
+    core.info("Cleaning up ...")
+    execSync("docker system prune -af", { stdio: [] })
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
